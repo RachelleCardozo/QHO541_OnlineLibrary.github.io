06d9edc3b733f927b9054b4d4a5d4afa7df7999b
RachelleCardozo <127194914+RachelleCardozo@users.noreply.github.com>
2024-06-11 20:47:34 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,17 +2,20 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const auth = async (req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer ', '');
     try {
+        const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findById(decoded.id);
+        const user = await User.findOne({ _id: decoded.id });
+
         if (!user) {
             throw new Error();
         }
+
+        req.token = token;
         req.user = user;
         next();
     } catch (err) {
-        res.status(401).json({ error: 'Please authenticate' });
+        res.status(401).json({ error: 'Please authenticate.' });
     }
 };
 
